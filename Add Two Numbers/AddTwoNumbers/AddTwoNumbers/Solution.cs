c1deb176312a024c0e34c011a6a67767f9c5ed0c
Vadim Mustafaev <vmustafaev@appulate.com>
2023-01-20 11:11:55 +0500

    better solution without recursion

--- Add Two Numbers/AddTwoNumbers/AddTwoNumbers/Solution.cs
+++ Add Two Numbers/AddTwoNumbers/AddTwoNumbers/Solution.cs
@@ -2,6 +2,32 @@ namespace AddTwoNumbers;
 
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        var head = new ListNode();
+        ListNode current = head;
+        int surplus = 0;
+
+        while (l1 != null || l2 != null || surplus != 0) {
+            int l1Digit = l1?.val ?? default;
+            int l2Digit = l2?.val ?? default;
+            int sum = l1Digit + l2Digit + surplus;
+            if (sum >= 10) {
+                sum -= 10;
+                surplus = 1;
+            }
+            else {
+                surplus = 0;
+            }
+
+            current.next = new ListNode(sum);
+            current = current.next;
+            l1 = l1?.next;
+            l2 = l2?.next;
+        }
+
+        return head.next;
+    }
+    
+    public ListNode AddTwoNumbersRecursion(ListNode l1, ListNode l2) {
         return SumNodes(l1, l2, 0);
     }
 
